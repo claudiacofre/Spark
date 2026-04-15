e8d3056c22467fb4675c53399b1a395e948dc870
Claudia Cofré <claudia.cofre@icloud.com>
2026-04-14 20:00:26 -0400

    ruta publish

--- src/routes/spark.router.js
+++ src/routes/spark.router.js
@@ -7,6 +7,7 @@ const router = Router();
 router.get('/sparks', getSparks); // Devuelve el JSON que busca script.js
 router.post('/sparks', postSpark); // Crea la chispa
 router.get('/feed', getFeed);  // Ver el Muro
+router.post('/publish', postSpark); // Postear en el muro
 router.get("/post/:id", getPostDetail);;  // Ver un comentario en especifico
 
  
