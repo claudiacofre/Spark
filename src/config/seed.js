b094934c413d4ce1fc80e60ae0ec5e450fd6d613
Claudia Cofré <claudia.cofre@icloud.com>
2026-04-15 19:46:57 -0400

    cambios en los mensajes en consola

--- src/config/seed.js
+++ src/config/seed.js
@@ -1,24 +1,41 @@
-// Conectar y sincronizar
 import sequelize from "./database.js";
 import Spark from "../models/spark.js";
+import User from "../models/user.js"; 
 
 const seedDatabase = async () => {
   try {
     await sequelize.authenticate(); // Conectar y sincronizar
+    console.log("Conexión establecida para el sembrado. 🌱");
 
-    // --- Limpieza ---
-    await Spark.destroy({ where: {}, truncate: true, cascade: true });  // 'truncate' borra todos los registros y reinicia los contadores (IDs)
+    await sequelize.sync({ force: false }); 
+    console.log("Tablas sincronizadas o creadas si no existían. 🔄");
+
+    // ---  Limpieza Total ---
+    await Spark.destroy({ where: {}, truncate: true, cascade: true });
+    await User.destroy({ where: {}, truncate: true, cascade: true });
     console.log("Base de datos limpiada. 🗑️");
 
-    // --- Carga ---
-    const initialSparks = [
-      { content: "¡Bienvenidos a Spark! Mi primera chispa. " },   // Dato de prueba (Chispa inicial)
+    // ---  Carga de Usuarios  ---
+    const initialUsers = [
+      { username: "Claudia", email: "claudia@spark.com", password: "password123" },
+      { username: "Eduardo", email: "eduardo@gmail.com", password: "secure456" },
+      { username: "Catalina", email: "catalina@icloud.com", password: "test789" }
     ];
 
-    await Spark.bulkCreate(initialSparks); // Insertar datos
-    console.log("Datos semilla cargados con éxito. ✅");
+    await User.bulkCreate(initialUsers);
+    console.log("Se han registrado 3 Usuarios semilla con éxito. ✅");
 
-    process.exit(0); // Cerrar el proceso exitosamente
+    // ---  Carga de Chispas ---
+    const initialSparks = [ // Dato de prueba (Chispas iniciales)
+      { content: "Probando que tal esta spark...", username: "Eduardo" }
+      { content: "¡Bienvenidos a Spark! Mi primera chispa.", username: "Claudia" },
+      
+    ];
+
+    await Spark.bulkCreate(initialSparks);
+    console.log("Chispas iniciales cargadas. ✨");
+
+    process.exit(0);
   } catch (error) {
     console.error("Error en el proceso de seed: ❌", error);
     process.exit(1);
