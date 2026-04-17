2f77a6c553a12c35a1b7fd86a4452099fc164923
Claudia Cofré <claudia.cofre@icloud.com>
2026-04-17 01:47:49 -0400

    añadido users/:id

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,12 +1,12 @@
-import { Router } from 'express';
+import { Router } from 'express'
+import User from '../models/User.js';
 import { getAllUsers, updateUser, deleteUser, getUserWithSparks, registerWithWelcomeSpark } from '../controllers/user.controller.js';
 
 const router = Router();
 
-router.get('/', getAllUsers);
-router.put('/:id', updateUser);    // Ruta para modificar
-router.delete('/:id', deleteUser); // Ruta para borrar
 router.post('/register', registerWithWelcomeSpark); // Para registrar un usuario nuevo (POST)
+router.put('/users/:id', updateUser);    // Ruta para modificar
+router.delete('/users/:id', deleteUser); // Ruta para borrar
 router.get('/users', getAllUsers); // Para ver a todos los usuarios (GET)
 router.get('/users/:username/sparks', getUserWithSparks); // Para ver a un usuario y sus publicaciones (GET)
 
