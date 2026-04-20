33c95218a13254df7a2017895ebafd4197480ea1
Claudia Cofré <claudia.cofre@icloud.com>
2026-04-20 01:54:58 -0400

    eliminacion de datos falsos de prueba

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,11 +1,10 @@
 import { Router } from 'express'
 import User from '../models/User.js';
-import { getAllUsers, updateUser, deleteUser, getUserWithSparks, getUsersJSON } from '../controllers/user.controller.js';
+import { getAllUsers, updateUser, deleteUser, getUserWithSparks } from '../controllers/user.controller.js';
 
 const router = Router();
 
 router.get('/', getAllUsers);                // GET /api/users
-router.get('/', getUsersJSON);
 router.get('/:username/sparks', getUserWithSparks); // GET /api/users/:username/sparks
 router.put('/:id', updateUser);             // PUT /api/users/:id
 router.delete('/:id', deleteUser);          // DELETE /api/users/:id
