39ea4a4eb958ebaffb7b5160a7b45a4c0540b42d
Claudia Cofré <claudia.cofre@icloud.com>
2026-04-23 00:42:48 -0400

    models

---
+++ src/models/spark.models.js
@@ -0,0 +1,48 @@
+import { Model, DataTypes } from 'sequelize';
+import sequelize from '../config/database.js'; // Conexión a la DB
+
+class Spark extends Model {}
+
+Spark.init({
+  // Identificador único (UUID es mejor para microblogging)
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+   // defaultValue: 'Claudia' --> Esto ayuda para que las chispas antiguas no queden vacías
+  },
+  // El contenido de la "Chispa"
+  content: {
+    type: DataTypes.STRING(300), // Límite un poco mayor que el clásico en microblogging.
+    allowNull: false,
+    validate: {
+      len: [1, 300] // Válida que no esté vacío ni se pase del límite. (No permite publicaciones vacías, ni más extensas)
+    }
+  }, 
+  // Contador de interacciones
+  likeCount: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0
+  },
+  // Para hilos (referencia a otra chispa)
+  parentId: {
+    type: DataTypes.UUID,
+    allowNull: true,
+    references: {
+      model: 'sparks', // Nombre de la tabla en la BdD
+      key: 'id'
+    }
+  }
+}, {
+  sequelize,
+  modelName: 'Spark',
+  tableName: 'sparks', // Fuerzo el nombre de la tabla
+  underscored: true,   // Para que use created_at en lugar de createdAt
+  timestamps: true  // Esto crea automáticamente 'createdAt' y 'updatedAt'
+});
+
+export default Spark;
---
+++ src/models/user.models.js
@@ -0,0 +1,23 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/database.js";
+
+const User = sequelize.define(
+  "User",
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    username: { type: DataTypes.STRING, allowNull: false, unique: true },
+    email: { type: DataTypes.STRING, allowNull: false, unique: true },
+    password: { type: DataTypes.STRING, allowNull: false },
+    role: { type: DataTypes.STRING, defaultValue: "user" },
+  },
+  {
+    underscored: true,
+    timestamps: true,
+  },
+);
+
+export default User;
